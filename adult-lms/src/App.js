48df5598af4762d9379728a95e6b0860d576103f
khalifa-pendrops <dikee5200@gmail.com>
2024-05-09 11:54:57 +0100

    weird changes on App.js. fixed and i hope this works sha

--- adult-lms/src/App.js
+++ adult-lms/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
-feature/login-admin-page
 import SignUp from './AdminSignUp';
-
 import Login from './Login';
-main
 
 function App() {
   return (
     <div className="App">
-feature/login-admin-page
   <SignUp />
-
 <Login />
-main
     </div>
   );
 }
