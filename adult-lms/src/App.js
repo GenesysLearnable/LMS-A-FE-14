fdc054324480e6d1a9ffaa2db68398c523f51206
khalifa-pendrops <dikee5200@gmail.com>
2024-05-10 07:14:55 +0100

    second commit

--- adult-lms/src/App.js
+++ adult-lms/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import Login from './Components/AdminLogin';
 
 import SignUp from './AdminSignUp';
-import Login from './Login';
+
 import Welcome from './component/welcome';
 import Header from './component/welcomeheader';
 
 export default function GoLearn() {
   return (
-    <div>
-      <Login />
     <div className="App">
   <SignUp />
 <Login />
