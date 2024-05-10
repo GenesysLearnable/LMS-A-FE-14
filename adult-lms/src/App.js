9cfeebf465d8851a780f8b48692ba2ed478322d7
khalifa-pendrops <dikee5200@gmail.com>
2024-05-10 12:54:11 +0100

    sixth commit

--- adult-lms/src/App.js
+++ adult-lms/src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import Signup from './Components/AdminSignUp';
-import './App.css';
-import Login from './Components/AdminLogin';
-import Welcome from './component/welcome';
-import Header from './component/welcomeheader';
+import React from "react";
+import Signup from "./Components/AdminSignUp";
+import "./App.css";
+import Login from "./Components/AdminLogin";
+import Welcome from "./component/welcome";
+import Header from "./component/welcomeheader";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
-    export default function GoLearn() {
-    
+export default function GoLearn() {
+    return (
+        <div className="App">
+            <Header />
+            <Welcome />
+            <Signup />
+            <Login />
+      </div>
+    // <Router>
+    //   <div className="App">
+    //     <Link>
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
 
-  return (
-<div className="App">
-  <Signup />
-<Login />
-      <Header />
-      <Welcome />
-    </div>
-  )
+    //       <Route path="/signup">
+    //         <Signup />
+    //       </Route>
+
+    //       <Route path="/login">
+    //         <Login />
+    //       </Route>
+
+    //       <Route path="/header">
+    //         <Header />
+    //       </Route>
+
+    //       <Route path="/welcome">
+    //         <Welcome />
+    //       </Route>
+    //     </Link>
+    //   </div>
+    // </Router>
+  );
 }
