c047057bb7c5f744e85c6f569f652f9fe5fb71e8
khalifa-pendrops <dikee5200@gmail.com>
2024-05-11 13:49:22 +0100

    tenth coommit

--- adult-lms/src/App.js
+++ adult-lms/src/App.js
@@ -1,44 +1,27 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
 import Signup from "./Components/AdminSignUp";
 import "./App.css";
 import Login from "./Components/AdminLogin";
 import Welcome from "./component/welcome";
 import Header from "./component/welcomeheader";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>Testing Routing</h1>} />)
+);
 
 export default function GoLearn() {
-    return (
-        <div className="App">
-            <Header />
-            <Welcome />
-            <Signup />
-            <Login />
-      </div>
-    // <Router>
-    //   <div className="App">
-    //     <Link>
-    //       <Route exact path="/">
-    //         <Home />
-    //       </Route>
-
-    //       <Route path="/signup">
-    //         <Signup />
-    //       </Route>
-
-    //       <Route path="/login">
-    //         <Login />
-    //       </Route>
-
-    //       <Route path="/header">
-    //         <Header />
-    //       </Route>
-
-    //       <Route path="/welcome">
-    //         <Welcome />
-    //       </Route>
-    //     </Link>
-    //   </div>
-    // </Router>
+  return (
+    <div className="App">
+      <Header />
+      <Welcome />
+      <Signup />
+      <Login />
+    </div>
   );
 }
