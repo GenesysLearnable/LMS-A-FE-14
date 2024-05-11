ed3dc638a08ba9cb2eea6d8ba1dc3af926451882
khalifa-pendrops <dikee5200@gmail.com>
2024-05-11 23:56:52 +0100

    student login page added

--- adult-lms/src/App.js
+++ adult-lms/src/App.js
@@ -10,6 +10,7 @@ import "./App.css";
 import Login from "./Components/AdminLogin";
 import Welcome from "./component/welcome";
 import Header from "./component/welcomeheader";
+import LoginForm from "./StudentLogin/StudentLogin";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route index element={<h1>Testing Routing</h1>} />)
@@ -20,6 +21,7 @@ export default function GoLearn() {
     <div className="App">
       <Header />
       <Welcome />
+      <LoginForm />
       <Signup />
       <Login />
     </div>
