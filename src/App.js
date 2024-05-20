2b7c0d06bcd04af90ab1ef4d336995a338c2e40f
Tochukwu Mezue <teejaymezue8@gmail.com>
2024-05-20 07:03:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
 import LoginForm from "./StudentLogin/StudentLogin";
-import StudentProfile from "./StudentScreens/StudentProfile";
-import ErrorPage from "./StudentScreens/ErrorPage";
-
-
-
 
 export default function GoLearn() {
 
     return (
       <div className="App">
         <LoginForm />
-        <StudentProfile />
-        <ErrorPage />
       </div>
     );
 }
