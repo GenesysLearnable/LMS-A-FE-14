f4523745c82f32758bbc78354da34532a6a2e5c5
khalifa-pendrops <dikee5200@gmail.com>
2024-05-23 10:49:02 +0100

    second conflict resolved

--- src/App.js
+++ src/App.js
@@ -19,19 +19,3 @@ export default function GoLearn() {
 }
 
 
-import React from "react";
-import "./App.css";
-import LoginForm from "./StudentLogin/StudentLogin";
-import LandingPage from "./Components/LandingPage";
-// import ExpMain from "./Reusables/ExploreMain";
-
-export default function GoLearn() {
-  return (
-    <div className="App">
-      <LoginForm />
-      <LandingPage />
-      {/* <ExpMain /> */}
-    </div>
-  );
-}
-
