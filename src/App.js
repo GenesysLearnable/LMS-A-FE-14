0e5c50ec77991e3283be604fb3cc39fe444bb07f
Onyenaucheya <chiomaruth79@gmail.com>
2024-05-22 22:55:46 -0700

    home page added but needs touches

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import LoginForm from "./StudentLogin/StudentLogin";
+import Studentschool from "./StudentScreens/Studentschool";
+import Studenthome from "./StudentScreens/Studenthome"
 // import LandingPage from "./Components/LandingPage";
 // import ExpMain from "./Reusables/ExploreMain";
 
@@ -8,6 +10,8 @@ export default function GoLearn() {
   return (
     <div className="App">
       <LoginForm />
+      <Studentschool />
+      <Studenthome />
       {/* <LandingPage /> */}
       {/* <ExpMain /> */}
     </div>
