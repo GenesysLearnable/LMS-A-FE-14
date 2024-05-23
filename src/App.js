340ad3625540d51a73c6419e633ea402e34ffbfd
khalifa-pendrops <dikee5200@gmail.com>
2024-05-23 10:41:01 +0100

    commit before pull

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import LoginForm from "./StudentLogin/StudentLogin";
-// import LandingPage from "./Components/LandingPage";
+import LandingPage from "./Components/LandingPage";
 // import ExpMain from "./Reusables/ExploreMain";
 
 export default function GoLearn() {
   return (
     <div className="App">
       <LoginForm />
-      {/* <LandingPage /> */}
+      <LandingPage />
       {/* <ExpMain /> */}
     </div>
   );
