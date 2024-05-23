e687eb81482ed860132a45e82b10d82e8e5000d5
khalifa-pendrops <dikee5200@gmail.com>
2024-05-23 17:01:27 +0100

    App.js edited and updated with landing page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
+
 import React from "react";
 import "./App.css";
-import LoginForm from "./StudentLogin/StudentLogin"
-
-export default function GoLearn() {
-
-    return (
-      <div className="App">
-      <LoginForm />
-      </div>
-    );
-}
-import React from "react";
-import "./App.css";
-import LoginForm from "./StudentLogin/StudentLogin";
 import LandingPage from "./Components/LandingPage";
-import SetPwd from "./ForgotPassword/SetPwd";
+
 
 export default function GoLearn() {
   return (
